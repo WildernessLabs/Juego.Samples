22f41d0b291737eec18926b195a8b6599ae70a87
Adrian Stevens <adrian.stevens@gmail.com>
2023-12-01 22:29:55 -0800

    Add Snake

---
+++ Source/Tetraminoes/MeadowApp.cs
@@ -0,0 +1,164 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Audio;
+using Meadow.Foundation.Graphics;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Tetraminos;
+using WildernessLabs.Hardware.Juego;
+
+namespace Tetramino
+{
+    // Change F7FeatherV2 to F7FeatherV1 for V1.x boards
+    public class MeadowApp : App<F7CoreComputeV2>
+    {
+        IJuegoHardware juego;
+        TetraminosGame game;
+        MicroGraphics graphics;
+        MicroAudio moveAudio;
+        MicroAudio effectsAudio;
+
+        GameState gameState = GameState.Ready;
+
+        enum GameState
+        {
+            Ready,
+            Playing,
+            GameOver
+        }
+
+        public override Task Initialize()
+        {
+            Console.WriteLine("Initialize...");
+
+            game = new TetraminosGame();
+
+            juego = Juego.Create();
+            juego.Left_LeftButton.Clicked += (s, e) => game.Left();
+            juego.Left_RightButton.Clicked += (s, e) => game.Right();
+            juego.Left_DownButton.Clicked += (s, e) => game.Down();
+
+            juego.Right_LeftButton.Clicked += (s, e) => game.Left();
+            juego.Right_RightButton.Clicked += (s, e) => game.Right();
+            juego.Right_DownButton.Clicked += (s, e) => game.Down();
+
+            juego.StartButton.Clicked += StartButton_Clicked;
+
+            graphics = new MicroGraphics(juego.Display)
+            {
+                CurrentFont = new Font12x16(),
+            };
+
+            moveAudio = new MicroAudio(juego.LeftSpeaker);
+            effectsAudio = new MicroAudio(juego.RightSpeaker);
+
+            game.Init(graphics, moveAudio, effectsAudio);
+
+            Console.WriteLine("Initialize complete");
+
+            return Task.CompletedTask;
+        }
+
+        public override Task Run()
+        {
+            Console.WriteLine("Run...");
+
+            juego.BlinkyLed.SetBrightness(1.0f);
+
+            DrawplashScreen();
+
+            return Task.CompletedTask;
+        }
+
+        private void StartButton_Clicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("StartButton_Clicked");
+
+            if (GameState.Ready == gameState)
+            {
+                gameState = GameState.Playing;
+                PlayGame();
+            }
+            else if (GameState.GameOver == gameState)
+            {
+                gameState = GameState.Ready;
+                DrawplashScreen();
+            }
+        }
+
+        void UpdateGame()
+        {
+            if (juego.Left_LeftButton.State == true)
+            {
+                game.Left();
+            }
+            else if (juego.Left_RightButton.State == true)
+            {
+                game.Right();
+            }
+            else if (juego.Left_UpButton.State == true)
+            {
+                game.Up();
+            }
+            else if (juego.Left_DownButton.State == true)
+            {
+                game.Down();
+            }
+            else if (juego.SelectButton.State == true)
+            {
+                //    game.Quit();
+            }
+
+            game.Update();
+        }
+
+        void DrawplashScreen()
+        {
+            graphics.Clear();
+            graphics.DrawText(160, 70, "Tetraminos", Color.Cyan, ScaleFactor.X3, HorizontalAlignment.Center);
+            graphics.DrawText(160, 140, "Press Start", Color.LawnGreen, ScaleFactor.X1, HorizontalAlignment.Center);
+            graphics.Show();
+        }
+
+        void DrawEndScreen()
+        {
+            /*
+            graphics.Clear();
+
+            if (game.Winner)
+            {
+                graphics.DrawText(160, 80, "You Win!", FrogItGame.FrogColor, ScaleFactor.X3, HorizontalAlignment.Center);
+                graphics.DrawText(160, 140, $"Your time: {game.GameTime:F1}s", FrogItGame.WaterColor, ScaleFactor.X1, HorizontalAlignment.Center);
+                graphics.DrawText(160, 160, $"Your died: {game.Deaths} time(s)", FrogItGame.WaterColor, ScaleFactor.X1, HorizontalAlignment.Center);
+            }
+            else
+            {
+                graphics.DrawText(160, 80, "Game Over", FrogItGame.FrogColor, ScaleFactor.X3, HorizontalAlignment.Center);
+            }
+
+            graphics.Show();
+            */
+        }
+
+        Task PlayGame()
+        {
+            var t = new Task(() =>
+            {
+                game.Reset();
+                while (true)
+                {
+                    UpdateGame();
+                    Thread.Sleep(0);
+                }
+                gameState = GameState.GameOver;
+                DrawEndScreen();
+            }, TaskCreationOptions.LongRunning);
+
+            t.Start();
+
+            return t;
+        }
+    }
+}
---
+++ Source/Tetraminoes/TetraminosGame.Renderer.cs
@@ -0,0 +1,110 @@
+﻿using Meadow.Foundation.Audio;
+using Meadow.Foundation.Graphics;
+using System;
+using System.Threading;
+
+namespace Tetraminos
+{
+    public partial class TetraminosGame
+    {
+        MicroAudio moveAudio;
+        MicroAudio effectsAudio;
+        MicroGraphics graphics;
+
+        int blockSize = 1;
+        int topIndent;
+        int leftIndent;
+
+        public void Init(MicroGraphics graphics, MicroAudio moveAudio, MicroAudio effectsAudio)
+        {
+            this.moveAudio = moveAudio;
+            this.effectsAudio = effectsAudio;
+            this.graphics = graphics;
+
+            graphics.Clear();
+            graphics.DrawText(0, 0, "Meadow Tetraminoes");
+            graphics.DrawText(0, 10, "v0.2.0");
+            graphics.Show();
+
+            leftIndent = 3;
+
+            graphics.CurrentFont = new Font8x12();
+
+            topIndent = graphics.CurrentFont.Height + 2;
+
+            //little hacky but works out nicely for the low res displays
+            blockSize = (graphics.Height - topIndent - 4) / 19;
+
+            Thread.Sleep(1000);
+        }
+
+        public void Update()
+        {
+            GameStateUpdate();
+            graphics.Clear();
+            DrawGameField(graphics);
+            DrawPreview(graphics);
+            graphics.Show();
+
+            Thread.Sleep(Math.Max(50 - Level, 0));
+        }
+
+        //ToDo - scale
+        void DrawPreview(MicroGraphics graphics)
+        {
+            //draw next piece
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (IsPieceLocationSet(i, j, NextPiece))
+                    {
+                        graphics.DrawRectangle(i * 2 + 54, j * 2, 2, 2);
+                    }
+                }
+            }
+        }
+
+        void DrawGameField(MicroGraphics graphics)
+        {
+            int xIndent = leftIndent + 2;
+            int yIndent = topIndent + 2;
+
+            graphics.DrawText(xIndent, 0, $"Lines: {LinesCleared}");
+
+            graphics.DrawRectangle(leftIndent,
+                topIndent,
+                4 + blockSize * 9,
+                4 + 19 * blockSize);
+
+            //draw current piece
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (IsPieceLocationSet(i, j, CurrentPiece))
+                    {
+                        //  graphics.DrawPixel(i, j);
+                        graphics.DrawRectangle((CurrentPiece.X + i) * blockSize + xIndent,
+                            (CurrentPiece.Y + j) * blockSize + yIndent,
+                            blockSize, blockSize, true, true);
+                    }
+                }
+            }
+
+            //draw gamefield
+            for (int i = 0; i < Width; i++)
+            {
+                for (int j = 0; j < Height; j++)
+                {
+                    if (IsGameFieldSet(i, j))
+                    {
+                        graphics.DrawRectangle((i) * blockSize + xIndent,
+                            (j) * blockSize + yIndent,
+                            blockSize, blockSize, true, true);
+                    }
+                }
+            }
+        }
+    }
+}
